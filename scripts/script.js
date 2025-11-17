a050b178b5de11664ecc6c7d4889296c16cf4a77
mattchu.dani <69618304+matfrm94wai@users.noreply.github.com>
2025-11-16 20:21:26 -1000

    added basic navigation scripting

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const loginButton = document.getElementById("loginSubmit");
+
+loginButton.addEventListener("click", function() {
+  window.location.replace("../pages/apply.html");
+})
+
+const applyButton = document.getElementById("loginSubmit");
+
+loginButton.addEventListener("click", function() {
+  window.location.replace("../pages/apply.html");
+})
+
+function applyPage() {
+  window.location.href = "../pages/apply.html";
+}
+
+function reviewPage() {
+  window.location.href = "../pages/review.html";
+}
+
+function signInPage() {
+  window.location.href = "../index.html";
+}
+
